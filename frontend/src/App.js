44e130b4f4ad18241fa48722fe278664f8666be8
mek3588 <mekbib35@gmial.com>
2023-08-29 16:11:03 +0300

    Updated dependencies and fixed linting issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import "./App.css"
-import Pages from "./components/pages/Pages"
+import "./App.css";
+import Pages from "./components/pages/Pages";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { setDataProduct } from "./components/redux/productSlide";
 
 function App() {
-  const dispatch = useDispatch()
-  const productData = useSelector((state)=>state.product)
-  console.log(productData)
- 
-  useEffect(()=>{
-    (async()=>{
-      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/Product`)
-      const resData = await res.json()
-      dispatch(setDataProduct(resData))
-    })()
-  },[])
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.product);
 
-  return (
-    
-  <Pages />)
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/Product`);
+        const resData = await res.json();
+        dispatch(setDataProduct(resData));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    })();
+  }, [dispatch]); // Added `dispatch` as a dependency
 
+  return <Pages />;
 }
 
-export default App
+export default App;
