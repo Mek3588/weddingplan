05ffa7d2039b1693118dd8ae83de70b729116a37
mek3588 <mekbib35@gmial.com>
2023-09-04 23:34:04 +0300

    Your commit message here

--- frontend/src/components/common/header/Header.jsx
+++ frontend/src/components/common/header/Header.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 import "./header.css";
-import { Link, useNavigate } from "react-router-dom"; 
+import { Link, useNavigate } from "react-router-dom";
 import logo from "./logo.png";
 import { logoutRedux } from "../../redux/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-hot-toast";
 import { BsFillArrowThroughHeartFill } from "react-icons/bs";
-import {BiUserCircle} from "react-icons/bi"
+import { BiUserCircle } from "react-icons/bi";
 
 const Header = () => {
   const [navList, setNavList] = useState(false);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const userData = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  
 
   const handleLogout = () => {
     dispatch(logoutRedux());
     toast("Logged out successfully");
   };
-  
 
   const nav = [
     {
@@ -49,21 +47,20 @@ const Header = () => {
     },
     {
       text: "Budget Track",
-      path: "/budgetrack", 
+      path: "/budgetrack",
     },
     {
       text: "payments",
-      path: "/payments", 
+      path: "/payments",
     },
     {
       text: "Vendors",
-      path: "/Product", 
+      path: "/Product",
     },
     {
       text: "planner",
       path: "/Planner",
     },
-
   ];
 
   return (
@@ -85,10 +82,8 @@ const Header = () => {
                   <Link to="/Newproduct" className="whitespace-nowrap cursor-pointer px-2">
                     New product
                   </Link>
-                  
                 </li>
               )}
-            
             </ul>
           </div>
           <div className="button flex">
@@ -96,41 +91,40 @@ const Header = () => {
               <span>2</span> My List
             </h4>
             {userData.image1 ? (
-              <button
-                className="btn1"
-                onClick={handleLogout}
-                    >
-                      <i className="fa fa-sign-out"></i>
-                      Logout ({userData.firstName})
-                    </button>
+              <>
+                <button className="btn1" onClick={handleLogout}>
+                  <i className="fa fa-sign-out"></i>
+                  Logout ({userData.firstName})
+                </button>
+                <div className="text-3xl cursor-pointer w-11 h-10 rounded-full overflow-hidden drop-shadow-md" id="imge">
+                  {userData.image1 ? (
+                    <img src={userData.image1} className="h-full w-full" alt="User Profile" />
                   ) : (
-                    <button
-                    onClick={() => {
-                      navigate('/login');
-                    }}
-                    className="btn1"   >
-                      <i className="fa fa-sign-out"></i>
-                      Login
-                    </button>
+                    <BiUserCircle />
                   )}
-            <div className="text-3xl cursor-pointer w-11 h-10 rounded-full overflow-hidden drop-shadow-md" id="imge">
-              {userData.image1 ? (
-                <img src={userData.image1} className="h-full w-full" />
-              ) : (
-                <BiUserCircle />
-              )}
-            </div>
-            <div style={{ fontSize: '2em', marginLeft: '10px' }}>
-              <BsFillArrowThroughHeartFill />
-            </div>
-            <div className="text-3xl cursor-pointer w-11 h-10 rounded-full overflow-hidden drop-shadow-md" id="imge">
-              {userData.image2 ? (
-                <img src={userData.image2} className="h-full w-full" />
-              ) : (
-                <BiUserCircle />
-              )}
-            </div>
-          
+                </div>
+                <div style={{ fontSize: '2em', marginLeft: '10px' }}>
+                  <BsFillArrowThroughHeartFill />
+                </div>
+                <div className="text-3xl cursor-pointer w-11 h-10 rounded-full overflow-hidden drop-shadow-md" id="imge">
+                  {userData.image2 ? (
+                    <img src={userData.image2} className="h-full w-full" alt="User Profile" />
+                  ) : (
+                    <BiUserCircle />
+                  )}
+                </div>
+              </>
+            ) : (
+              <>
+                <button onClick={() => navigate('/login')} className="btn1">
+                  <i className="fa fa-sign-out"></i>
+                  Login
+                </button>
+                <div className="text-3xl cursor-pointer w-11 h-10 rounded-full overflow-hidden drop-shadow-md" id="imge">
+                  <BiUserCircle />
+                </div>
+              </>
+            )}
           </div>
 
           <div className="toggle">
